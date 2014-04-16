466baca79f15e2d98cebe595bd87e79f1ce1cfeb
hung-phan <colorvisavn@gmail.com>
2014-04-16 11:06:18 +0700

    Update test-create-file.js

--- test/test-creation.js
+++ test/test-creation.js
@@ -27,7 +27,7 @@ describe('angular-with-browserify generator', function () {
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            cssFramework: 'SASSBootstrap'
         });
         this.app.options['skip-install'] = true;
         this.app.run({}, function () {
