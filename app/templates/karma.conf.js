6915ec3723855178a5b0090b2e7928f7dc64c818
hung-phan <colorvisavn@gmail.com>
2014-05-31 15:42:47 +0700

    modify karma config

--- app/templates/karma.conf.js
+++ app/templates/karma.conf.js
@@ -6,20 +6,19 @@ module.exports = function(config) {
     // base path, that will be used to resolve files and exclude
     basePath: '..',
 
-
     // frameworks to use
     frameworks: ['jasmine', 'browserify'],
 
     // list of files / patterns to load in the browser
     files: [
-      'test/specs/*.js',
-      'app/partials/**/*.html'
+      'app/**/*.spec.js',
+      'app/src/**/*.html',
     ],
 
     // Add browserify to preprocessors
     preprocessors: {
-      'test/specs/*.js': ['browserify'],
-      'app/partials/**/*.html': 'ng-html2js'
+      'app/**/*.spec.js': ['browserify'],
+      'app/src/**/*.html': 'ng-html2js'
     },
 
     ngHtml2JsPreprocessor: {
@@ -34,23 +33,18 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -62,10 +56,8 @@ module.exports = function(config) {
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     browsers: ['PhantomJS'],
 
-
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
-
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
