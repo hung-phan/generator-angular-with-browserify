fa1f1e4119467808c268ca600c939420e7614315
hung-phan <colorvisavn@gmail.com>
2014-06-02 00:53:39 +0700

    fix grunt bug

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function(grunt) {
             browserify: {
                 files: [
                     '<%%= yeoman.app %>/src/**/*.js',
-                    '<%%= yeoman.app %>/src/**/*.spec.js',
+                    '!<%%= yeoman.app %>/src/**/*.spec.js',
                     '!<%%= yeoman.app %>/src/main.js',
                     '!<%%= yeoman.app %>/src/spec.js'
                 ],
