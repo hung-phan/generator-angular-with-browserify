0e99238efcd0c7129eeac7aec2e5790a7e2e68d3
hung-phan <colorvisavn@gmail.com>
2014-06-01 23:41:07 +0700

    add template for directive

--- directive/index.js
+++ directive/index.js
@@ -8,10 +8,15 @@ var DirectiveGenerator = module.exports = function DirectiveGenerator(args, opti
   yeoman.generators.NamedBase.apply(this, arguments);
 
   console.log('You called the directive subgenerator with the argument ' + this.name + '.');
+  this.uppercaseName = this.name.charAt(0).toUpperCase() + this.name.slice(1);
 };
 
 util.inherits(DirectiveGenerator, yeoman.generators.NamedBase);
 
 DirectiveGenerator.prototype.files = function files() {
-  this.template('directive-template.js', 'app/scripts/directives/' + this.name + '-directive.js');
+  var prefix = 'app/src/' + this.name + '/';
+  this.mkdir(prefix);
+  this.template('directive-template.js'       , prefix + this.name + '.js');
+  this.template('directive-template.spec.js'  , prefix + this.name + '.spec.js');
+  this.template('directive-template.tpl.html' , prefix + this.name + '.tpl.html');
 };
