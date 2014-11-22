bd872f5b90fea6214f9641239d45f34eea93e539
hung-phan <colorvisavn@gmail.com>
2014-11-22 13:49:39 +0700

    Update website title

--- app/templates/test/e2eSpecs/angularE2Espec.js
+++ app/templates/test/e2eSpecs/angularE2Espec.js
@@ -2,6 +2,6 @@ describe('frontpage', function() {
     var ptor = protractor.getInstance();
     it('should get the title', function() {
         ptor.get('/#');
-        expect(ptor.getTitle()).toBe('generator-angular-with-require');
+        expect(ptor.getTitle()).not.toBe('dummy name');
     });
 });
